6df0ee6b35caea23e0c6b328d3b9ab0a8b0c2884
canw <cdzwm@heinfo.net>
2011-11-24 16:55:24 +0800

    . rename 'README' to 'Readme' . create directory src . add file gdy_server.js wq

---
+++ src/gdy_server.js
@@ -0,0 +1,11 @@
+var http = require('http');
+var host = '0.0.0.0';
+var port = 10086;
+
+var server = http.createServer(function (req, res){
+	res.end('Welcome to gdy v0.0.1\n', 'utf8');
+});
+
+server.listen(port, host);
+console.log('Server is running at ' + host + ' ' + port);
+
