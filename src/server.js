ef192ee14883ec4ada891b52d35c0e855caf03a7
canw <nodejs@live.cn>
2011-11-30 11:16:41 +0800

    remove SIGINIT event

--- src/server.js
+++ src/server.js
@@ -5,7 +5,3 @@ var gameserver = require('./lib/gameserver');
 gameserver.on('start', function(){
 	console.log('server ' + gameserver.serverconf.version + ' started.');
 });
-
-process.on('SIGINT', function (){
-	console.log('SIGINT');
-});
