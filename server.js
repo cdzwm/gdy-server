6e159b0807e03bc918ba281f31afadfe7675f016
canw <nodejs@live.cn>
2011-11-30 17:32:06 +0800

    Fix configuration file path bug.

--- server.js
+++ server.js
@@ -5,3 +5,5 @@ var gameserver = require('./lib/gameserver');
 gameserver.on('start', function(){
 	console.log('server ' + gameserver.serverconf.version + ' started.');
 });
+
+gameserver.startServer();
