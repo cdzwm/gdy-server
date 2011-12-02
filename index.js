7644c85398a0d862662e5bcdf6fede98e34be1f5
canw <nodejs@live.cn>
2011-12-02 16:10:59 +0800

    create mysql database gdydb

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ server.on('start', function(){
 	console.log('server ' + server.serverconf.version + ' started.');
 });
 
-server.on('newplayer', function(player){
-	console.log(player);
+server.on('newplayer', function(userkey){
+	server.getPlayer(userkey).dumpPlayer();
 });
+
 server.startServer();
