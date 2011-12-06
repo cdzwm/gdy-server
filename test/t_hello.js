1a9db933df6b1a2283d72449596f2843e07f5672
canw <nodejs@live.cn>
2011-12-06 17:27:47 +0800

    implement mq.js

--- test/t_hello.js
+++ test/t_hello.js
@@ -3,13 +3,22 @@ var net = require('net');
 var msg_begin = "GDY_MSG_BEGIN{";
 var msg_end="}GDY_MSG_END";
 
-var hello = {"cmd": "HELLO"};
+var hello1 = {"cmd": "HELLO{*}", "description":"handshake"};
+var hello2 = {"cmd": "HELLO", "description":"handshake", "test":"a test"};
+
 
 var client = net.connect(10086, '127.0.0.1', function(){
-	setInterval(helloAgain, 2000);
+	setInterval(helloAgain1, 1000);
+	setInterval(helloAgain2, 1500);
 });
 
-function helloAgain(){
-	client.write(msg_begin + JSON.stringify(hello) + msg_end);
+function helloAgain1(){
+	hello1.cmd =  "HELLO" + Math.round((1000*Math.random())).toString();
+	client.write(msg_begin + JSON.stringify(hello1) + msg_end);
+}
+
+function helloAgain2(){
+	for(var i=0;i<3;i++)
+		client.write(msg_begin + JSON.stringify(hello2) + msg_end);
 }
 
