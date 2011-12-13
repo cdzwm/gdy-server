3c6b0f39712733d65c1cf413cc376f34a2e0cfa1
canw <nodejs@live.cn>
2011-12-13 14:36:20 +0800

    Add session HELLO function.

--- test/t_hello.js
+++ test/t_hello.js
@@ -8,8 +8,10 @@ var hello2 = {"cmd": "HELLO", "description":"handshake", "test":"a test"};
 
 
 var client = net.connect(10086, '127.0.0.1', function(){
-	setInterval(helloAgain1, 1000);
-	setInterval(helloAgain2, 1500);
+	//setTimeout(helloAgain1, 1000);
+	setInterval(helloAgain2, 100);
+	client.setEncoding('utf8');
+	client.on('data', onReceiveMessage);
 });
 
 function helloAgain1(){
@@ -18,9 +20,13 @@ function helloAgain1(){
 }
 
 function helloAgain2(){
-	for(var i=0;i<3;i++){
+	for(var i=0;i<1;i++){
+		hello2.cmd =  "HELLO" + Math.round((1000*Math.random())).toString();
 		hello2.id = i;
 		client.write(msg_begin + JSON.stringify(hello2) + msg_end);
 	}
 }
 
+function onReceiveMessage(data){
+	console.log(data);
+}
