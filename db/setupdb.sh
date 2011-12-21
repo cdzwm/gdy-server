86d2ffb533faa4a3bf03c4827693f4142ad74779
canw <nodejs@live.cn>
2011-12-21 21:01:29 +0800

    Add a sql statement for create player table

--- db/setupdb.sh
+++ db/setupdb.sh
@@ -1,5 +1,17 @@
 mysql -uroot -p <<END
-create database gdydb;
+/*create database gdydb;*/
 use gdydb;
-create table player(id int, name text, nickname text);
+create table player(
+	id int, 
+	name text, 
+	nickname text,
+	description text,
+	sex text,
+	money int,
+	lastLoginTime datetime,
+	lastLogoutTime datetime,
+	loginTimes int,
+	level int,
+	blocked int
+);
 END
