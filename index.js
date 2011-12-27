b91dbb212cd7935931c132aa038ecc1378a7d469
canw <nodejs@live.cn>
2011-12-27 14:29:23 +0800

    add database configuration function from serverconf.json

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var server = require('./lib/server');
 
 // catch ready event
 server.on('ready', function(){
-	console.log('server ' + server.serverconf.version + ' started.');
+	console.log('server ' + global.serverconf.version + ' started.');
 });
 
 // start server
