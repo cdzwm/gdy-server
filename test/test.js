b3d2b0db24076be6ff73faa31c889215ffac1a54
canw <nodejs@live.cn>
2011-12-28 21:31:46 +0800

    Add test for poker.js

---
+++ test/test.js
@@ -0,0 +1,76 @@
+var poker = require("./lib/poker");
+var test_cases = [
+// 单牌测试
+[
+{name : "黑桃A", type : "spades", point : 14, next : 2, prev: 13}
+],
+[
+{name : "黑桃2", type : "spades", point : 15, prev : 1}
+],
+[
+{name : "黑桃3", type : "spades", point : 3, next : 4}
+],
+[
+{name : "黑桃4", type : "spades", point : 4, next : 5, prev : 3}
+],
+[
+{name : "红桃Q", type : "hearts", point : 12, next : 26, prev : 24}
+],
+[
+{name : "红桃K", type : "hearts", point : 13, next : 14, prev : 25}
+],
+[
+{name : "小王", type : "joker", point : 16, next : 54}
+],
+[
+{name : "大王", type : "joker", point : 17, prev : 53}
+],
+
+// 两张牌测试
+[
+{name : "黑桃A", type : "spades", point : 14, next : 2, prev: 13},
+{name : "黑桃2", type : "spades", point : 15, prev : 1}
+],
+[
+{name : "黑桃2", type : "spades", point : 15, prev : 1},
+{name : "黑桃2", type : "spades", point : 15, prev : 1}
+],
+[
+{name : "黑桃3", type : "spades", point : 3, next : 4},
+{name : "黑桃2", type : "spades", point : 15, prev : 1}
+],
+[
+{name : "黑桃4", type : "spades", point : 4, next : 5, prev : 3},
+{name : "小王", type : "joker", point : 16, next : 54}
+],
+[
+{name : "红桃Q", type : "hearts", point : 12, next : 26, prev : 24},
+{name : "大王", type : "joker", point : 17, prev : 53}
+],
+[
+	{name : "黑桃A", type : "spades", point : 14, next : 2, prev: 13},
+	{name : "红桃A", type : "hearts", point : 14, next : 15, prev : 26},
+	{name : "梅花A", type : "diamonds", point :14 , next : 28, prev : 39},
+	{name : "方块A", type : "clubs", point :14 , next : 41, prev : 52}
+]
+
+];
+
+var mycards = [];
+for( var i =1 ; i<54; i++)
+	mycards.push(i);
+
+var result = poker.shuffle(mycards);
+var thecards = poker.getCards();
+
+for(var card in thecards){
+	var r = poker.getSuits([thecards[card]]);
+	if( r.length > 0 ){
+		for( var s in r ){
+			console.log("Ok : ",  r[s]);
+		}
+	}
+	else{
+		console.log("Ieal: ", thecards[card]);
+	}
+}
---
+++ test/test_redis.js
@@ -0,0 +1,17 @@
+var redis = require("redis"),
+    client = redis.createClient(6379, "133.109.55.198");
+
+client.auth("redishereneedpwd");
+client.on("error", function (err) {
+    console.log("Error " + err);
+});
+
+client.set("string key", "string val", redis.print);
+client.hset("hash key", "hashtest 1", "some value", redis.print);
+client.hset(["hash key", "hashtest 2", "some other value"], redis.print);
+client.hkeys("hash key", function (err, replies) {
+    console.log(replies.length + " replies:");
+    replies.forEach(function (reply, i) {
+        console.log("    " + i + ": " + reply);
+    });
+});
