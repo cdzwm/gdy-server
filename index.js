0fe836f516e599b7c72465c8e4fe0611774c89bc
canw <nodejs@live.cn>
2011-12-31 08:55:25 +0800

    add lib/comm/debug.js

--- index.js
+++ index.js
@@ -1,34 +1,7 @@
 //module name: index
 //description: server entry point.
 
-global.DEBUG = (process.env["GDY_DEBUG"] == 1);
-global.DBG_LOG = function(arg1, arg2){
-	if( DEBUG ){
-		var tag_message = "", tag_name="GDY - ", message="";
-		if( arguments.length == 2 ){
-			switch(arg1.toLowerCase()){
-				case "i":
-					tag_message = "Info: ";
-					break;
-				case "d":
-					tag_message = "Debug: ";
-					break;
-				case "e":
-					tag_message = "Error: ";
-					break;
-				default:
-					tag_name="";
-					message = arg1;
-					break;
-			}
-			message += tag_name + tag_message  + arg2;
-		}
-		else{
-			message = arg1;
-		}
-		console.log(message);
-	}
-}
+require("./lib/comm/debug");
 
 var server = require('./lib/server');
 
