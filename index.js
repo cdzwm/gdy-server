699a21cf953cd56c36570ca980b987b120a719b8
canw <nodejs@live.cn>
2012-02-29 15:10:52 +0800

    refactory

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 //module name: index
 //description: server entry point.
 var debug = require("./lib/comm/debug")
-	,myutil = require("./lib/comm/util")
 	,server = require('./lib/server');
 
 // ready event
 server.on('ready', function(){
-	DBG_LOG("i", 'GDY server ' + server.serverconf.version + ' started.');
+	DBG_LOG("i", 'GDY server ' + server.version() + ' started.');
 });
 
 // start server
